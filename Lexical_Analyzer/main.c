2c99ae6098314c895fc920cc3d79440af529a9cb
Enzo Oliveira <ero2003730@gmail.com>
2023-11-02 14:35:35 -0300

    Arrumando problema comentario, e adicionando comentarios

--- Lexical_Analyzer/main.c
+++ Lexical_Analyzer/main.c
@@ -26,7 +26,9 @@ int main(int argc, char *argv[])
     // Aloca memória para o buffer
     allocate_buffer(&lexema);
 
+    // Matriz principal dos estados
     int matriz[13][21] = {
+        // Colunas: letra numero < > = + - * / ! ; , ( ) [ ] { } espaço \n \0
         {1, 2, 3, 3, 3, 4, 4, 4, 7, 6, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0},                       // estado q0 (inicial)
         {1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},   // estado q1
         {10, 2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},   // estado q2
@@ -41,14 +43,16 @@ int main(int argc, char *argv[])
         {11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11},  // estado q11 (erro)
         {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}}; // estado de transicao para '!=' pois estava dando erro
 
+    // Aariaveis auxiliares
     char c;
     int estado = 0, novo_estado = 0;
     int coluna = 0;
     int i = 0;
-    int lines = 1; // inicializa a contagem de linhas
+    int lines = 1; // Inicializa a contagem de linhas
 
     initHashTable(); // Inicializando o Hash
 
+    // Loop principal que fica até encontrar EOF
     while ((c = get_next_char(file)) != EOF)
     {
         if (c == '\n')
@@ -56,23 +60,36 @@ int main(int argc, char *argv[])
             lines++;
         }
 
+        // Loop até encontrar um lexema válido (ou um erro)
         while (1)
         {
+            // Pega a coluna em que o caractere se refere
             coluna = get_position(c);
 
+            // Se coluna == -1, significa que não foi encontrado o caractere
+            // Logo não é um caractere pertencente a linguagem C-
             if (coluna == -1)
             {
                 printf("Erro: caractere: '%c', na linha %d não eh válido\n", c, lines);
                 return -1;
             }
 
+            // Pegar o estado em que o caractere está transitando para
             novo_estado = matriz[estado][coluna];
 
+            // Verificação de comentário, se o estado atual for = 7, e o estado que esta
+            // sendo transitado para for = 8, então entramos no loop de comentario
             if (estado == 7 && novo_estado == 8)
             {
+                // A primeira '/' do comentario é colocada no lexema, pois não satisfaz
+                // a condição de estado = 7 e novo_estado = 8, então para não pegá-la,
+                // iremos colocar um \0 para zerar
+                lexema[aux].lexemaBuffer[0] = '\0';
+
                 int comentarioFechado = 0;
                 c = get_next_char(file);
 
+                // While que ira pegar caractere por caractere dentro do comentario
                 while (!comentarioFechado && c != EOF)
                 {
                     if (c == '\n')
@@ -80,50 +97,69 @@ int main(int argc, char *argv[])
                         lines++;
                     }
 
+                    // Caso o caractere seja = '*', então pegar o proximo char
                     if (c == '*')
                     {
                         c = get_next_char(file);
+
+                        // Caso o próxio caractere seja igual a '/', então, acabou a
+                        // condição de comentário, logo sair
                         if (c == '/')
                         {
                             comentarioFechado = 1;
+                            // Pegar o caractere seguinte, pois o atual é o '/' e ele
+                            // não pode ser adicionado como lexema
+                            c = get_next_char(file);
                             break;
                         }
                     }
                     c = get_next_char(file);
                 }
 
+                // Se não encontrar o final do comentário, '*/' isso é um erro, logo printar erro
                 if (!comentarioFechado)
                 {
                     printf("Erro: comentario nao fechado antes do fim do arquivo na linha %d\n", lines);
                     return -1;
                 }
             }
+
+            // Else para caso não satisfaça comentário
             else
             {
+                // Verificar se estou no estado inicial, final ou de erro
                 if (novo_estado == 0 || novo_estado == 10 || novo_estado == 11)
                 {
+                    // Caso esteja no estado final (10), reprocessar o último caractere, para ser analisado
                     if (novo_estado == 10)
                     {
                         reprocess = 1;
                     }
 
+                    // Dar break no loop de dentro para analisar se é estado final ou erro
                     break;
                 }
 
+                // Se não for nenhum desses estados, então apenas adicionar o caractere no lexema
                 lexema[aux].lexemaBuffer[i] = c;
                 i++;
                 c = get_next_char(file);
 
+                // If para verificação de caso o caractere seguinte ser o EOF, então o que tiver no lexema
+                // precisa ainda ser salvo
                 if (c == EOF)
                 {
                     lexema[aux].linha = lines;
 
+                    // Se veio do estado 1, então é uma palavra, verificar, com complexidade > 0(n) se é palavra
+                    // reservada ou se é um identificador
                     if (estado == 1)
                     {
                         lexema[aux].lexemaBuffer[i] = '\0';
                         identify_keyword_or_id_using_hash(lexema[aux].lexemaBuffer, &lexema, aux);
                         aux++;
                     }
+                    // Se não for um identificador apenas mandar para analisar o token
                     else
                     {
                         lexema[aux].lexemaBuffer[i] = '\0';
@@ -137,16 +173,21 @@ int main(int argc, char *argv[])
             }
         }
 
+        // Se o próximo estado for o final, então quer dizer que chegou numa condição que o lexema precisa
+        // ser classificado
         if (novo_estado == 10)
         {
             lexema[aux].linha = lines;
 
+            // Se veio do estado 1, então é uma palavra, verificar, com complexidade > 0(n) se é palavra
+            // reservada ou se é um identificador
             if (estado == 1)
             {
                 lexema[aux].lexemaBuffer[i] = '\0';
                 identify_keyword_or_id_using_hash(lexema[aux].lexemaBuffer, &lexema, aux);
                 aux++;
             }
+            // Se não for um identificador apenas mandar para analisar o token
             else
             {
                 lexema[aux].lexemaBuffer[i] = '\0';
@@ -154,6 +195,9 @@ int main(int argc, char *argv[])
                 aux++;
             }
         }
+
+        // Estado de erro. Para esse caso especifico, a unica condicao de erro da matriz é caso seja digitado
+        // '!' e não seguir de '='
         else if (novo_estado == 11)
         {
             printf("Erro na linha %d: O caractere '!' foi encontrado, mas deve ser seguido imediatamente por um '=' para formar o operador '!=' válido.\n", lines);
